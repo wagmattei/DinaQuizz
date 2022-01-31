3446af80cdc45db9f544bd92dc2101b87c8da44e
wagmattei <wagmattei@wattei.com.br>
2022-01-31 13:51:18 -0300

    feat: created Result screen

--- app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
+++ app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
@@ -1,12 +1,22 @@
 package com.wagmattei.dynaquiz.ui.quizquestion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wagmattei.dynaquiz.R
+import com.wagmattei.dynaquiz.ui.quizresult.QuizResultActivity
+import kotlinx.android.synthetic.main.activity_quizz_question.*
 
 class QuizzQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizz_question)
+
+        // Only for teste
+        btn_sendAnswer.setOnClickListener{
+            startActivity(Intent(this, QuizResultActivity::class.java))
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/com/wagmattei/dynaquiz/ui/quizresult/QuizResultActivity.kt
@@ -0,0 +1,15 @@
+package com.wagmattei.dynaquiz.ui.quizresult
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wagmattei.dynaquiz.R
+import kotlinx.android.synthetic.main.activity_quizz_question.*
+
+class QuizResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_result)
+
+    }
+}
