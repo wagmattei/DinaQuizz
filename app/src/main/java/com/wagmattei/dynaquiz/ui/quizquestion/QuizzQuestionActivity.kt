746e83a60eef51d097806be9dd62d6b25425b14e
wagmattei <wagmattei@wattei.com.br>
2022-01-31 17:35:17 -0300

    feat: improve Alert for success and error answer

--- app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
+++ app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
@@ -3,9 +3,15 @@ package com.wagmattei.dynaquiz.ui.quizquestion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import com.wagmattei.dynaquiz.R
 import com.wagmattei.dynaquiz.ui.quizresult.QuizResultActivity
 import kotlinx.android.synthetic.main.activity_quizz_question.*
+import kotlinx.android.synthetic.main.dialog_correct.view.*
+import kotlinx.android.synthetic.main.teste.view.*
 
 class QuizzQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +20,29 @@ class QuizzQuestionActivity : AppCompatActivity() {
 
         // Only for teste
         btn_sendAnswer.setOnClickListener{
-            startActivity(Intent(this, QuizResultActivity::class.java))
-            finish()
+           // startActivity(Intent(this, QuizResultActivity::class.java))
+           // finish()
+
+            showDialogAlert(R.layout.dialog_correct)
+            showDialogAlert(R.layout.dialog_incorrect)
+        }
+
+
+    }
+
+    private fun showDialogAlert(layout : Int) {
+
+        val view = View.inflate(this@QuizzQuestionActivity, layout, null)
+
+        val builder = AlertDialog.Builder(this@QuizzQuestionActivity)
+        builder.setView(view)
+
+        val dialog = builder.create()
+        dialog.show()
+        dialog.setCancelable(false)
+
+        view.dialog_button.setOnClickListener {
+            dialog.dismiss()
         }
 
     }
