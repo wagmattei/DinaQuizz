b63045d53e9435d67daef22e50f93e952992870a
wagmattei <wagmattei@wattei.com.br>
2022-01-31 10:26:18 -0300

    feat: created login screen in MainActivity

--- app/src/main/java/com/wagmattei/dinaquizz/ui/main/MainActivity.kt
+++ app/src/main/java/com/wagmattei/dinaquizz/ui/main/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.wagmattei.dinaquizz
+package com.wagmattei.dinaquizz.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.wagmattei.dinaquizz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hideActionBar()
+
+    }
+
+    private fun hideActionBar() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide();
     }
 }
