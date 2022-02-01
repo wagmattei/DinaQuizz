c1a53dd576059e1f50818d24b8d9da682f8b629b
wagmattei <wagmattei@wattei.com.br>
2022-02-01 10:13:34 -0300

    remove teste.xml file

--- app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
+++ app/src/main/java/com/wagmattei/dynaquiz/ui/quizquestion/QuizzQuestionActivity.kt
@@ -1,17 +1,13 @@
 package com.wagmattei.dynaquiz.ui.quizquestion
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.wagmattei.dynaquiz.R
-import com.wagmattei.dynaquiz.ui.quizresult.QuizResultActivity
 import kotlinx.android.synthetic.main.activity_quizz_question.*
 import kotlinx.android.synthetic.main.dialog_correct.view.*
-import kotlinx.android.synthetic.main.teste.view.*
+
 
 class QuizzQuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +15,9 @@ class QuizzQuestionActivity : AppCompatActivity() {
         setContentView(R.layout.activity_quizz_question)
 
         // Only for teste
-        btn_sendAnswer.setOnClickListener{
-           // startActivity(Intent(this, QuizResultActivity::class.java))
-           // finish()
+        btn_sendAnswer.setOnClickListener {
+            // startActivity(Intent(this, QuizResultActivity::class.java))
+            // finish()
 
             showDialogAlert(R.layout.dialog_correct)
             showDialogAlert(R.layout.dialog_incorrect)
@@ -30,7 +26,7 @@ class QuizzQuestionActivity : AppCompatActivity() {
 
     }
 
-    private fun showDialogAlert(layout : Int) {
+    private fun showDialogAlert(layout: Int) {
 
         val view = View.inflate(this@QuizzQuestionActivity, layout, null)
 
